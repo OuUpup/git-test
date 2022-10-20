e4ff4fbffab08071439d67511f7d719329506270
ou <ou.@qq.com>
2022-10-20 10:36:41 +0800

    first commit!

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Ou
+ * @create 2022-10-20 10:27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
